822cf87f087d86d2c5b5acedf36d3cd882179681
jrogers-dev <JRogers.dev@gmail.com>
2020-12-21 18:59:45 -0800

    Added a very simple (and incomplete) FetchAdapter class for simplifying fetches

--- src/index.js
+++ src/index.js
@@ -53,3 +53,31 @@ class Position {
 class Card {
 
 }
+
+class FetchAdapter {
+  static fetchData(url) {
+    return fetch(url).then(response => response.json());
+  }
+
+  static postData(url, data) {
+    return fetch(
+      url,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+      }
+    ).then(response => response.json());
+  }
+
+  static updateData(url, data) {
+
+  }
+
+  static deleteData(url) {
+
+  }
+}
