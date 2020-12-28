e82663b82b26395af14160e6b60ebcea1c60abb2
jrogers-dev <JRogers.dev@gmail.com>
2020-12-28 02:19:31 -0800

    Modified Game class constructor to be more robust and added a destroy method. Filled in the destroyData method of class FetchAdapter. Added a new method to class Dementia for creating a new game, which now sends a fetch call to create a new Game object on the rails backend, creates a javascript object from the response, then calls displaySetup. This newGame method is now called in the global click listener instead of displayLanding.

--- src/index.js
+++ src/index.js
@@ -3,21 +3,32 @@
 class Dementia {
   static marqueeContainer() {
     this.mc ||= document.querySelector("#marquee");
-    console.log(this.mc);
+    //console.log(this.mc);
     return this.mc;
   }
 
   static fieldContainer() {
     this.fc ||= document.querySelector('#field');
-    console.log(this.fc);
+    //console.log(this.fc);
     return this.fc;
   }
 
   static bottomContainer() {
     this.bc ||= document.querySelector('#bottom');
-    console.log(this.bc);
+    //console.log(this.bc);
     return this.bc;
   }
+
+  static newGame() {
+    this.game = new Game();
+    this.displaySetup();
+  }
+
+  static setupGame() {
+
+  }
+
+
 
   static displayLanding() {
     let spanBanner = document.createElement("span");
@@ -30,7 +41,6 @@ class Dementia {
     btnNewGame.id = "btnNewGame";
     btnNewGame.textContent = "Start Game";
     btnNewGame.classList.add("bg-blue-500", "rounded", "px-2");
-    console.log(btnNewGame);
     Dementia.bottomContainer().appendChild(btnNewGame);
   }
 
@@ -43,14 +53,12 @@ class Dementia {
     setupBanner.id = "setupBanner";
     setupBanner.textContent = "Game Setup";
     setupBanner.classList.add("justify-center", "text-4xl");
-    console.log(setupBanner);
     Dementia.marqueeContainer().appendChild(setupBanner);
 
     let btnStartGame = document.createElement("button");
     btnStartGame.id = "btnStartGame";
     btnStartGame.textContent = "Let's Go!";
     btnStartGame.classList.add("bg-green-500", "rounded", "px-2");
-    console.log(btnStartGame);
     Dementia.bottomContainer().appendChild(btnStartGame);
   }
 
@@ -63,7 +71,6 @@ class Dementia {
     gameBanner.id = "gameBanner";
     gameBanner.textContent = "Remember Not to Forget!";
     gameBanner.classList.add("flex", "justify-center", "text-4xl");
-    console.log(gameBanner);
     Dementia.marqueeContainer().appendChild(gameBanner);
   }
 }
@@ -71,19 +78,29 @@ class Dementia {
 
 class Game {
   constructor() {
-    FetchAdapter.postData("http://localhost:3000/games", {})
-      .then(result => { this.id = result.id });
-    this.state = 0;
+    FetchAdapter.postData("http://localhost:3000/games", {state: 0})
+      .then(result => {
+         this.id = result.data.id;
+         this.state = result.data.attributes.state;
+      })
+      .catch(err => console.log(err))
+    ;
   }
 
   static all() {
     FetchAdapter.fetchData("http://localhost:3000/games")
+      .then(result => console.log(result.data));
+  }
+
+  destroy() {
+    FetchAdapter.destroyData(`http://localhost:3000/games/${this.id}`)
       .then(result => console.log(result));
+    this.id = null;
+    this.state = null;
   }
 }
 
 class Player {
-
 }
 
 class Board {
@@ -122,8 +139,17 @@ class FetchAdapter {
 
   }
 
-  static deleteData(url) {
-
+  static destroyData(url) {
+    return fetch(
+      url, 
+      {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        }
+      }
+    );
   }
 }
 
@@ -137,7 +163,7 @@ document.addEventListener('click', function(e) {
   console.dir(e.target)
 
   if (e.target.id == "btnNewGame") {
-    Dementia.displaySetup();
+    Dementia.newGame();
   }
   else if (e.target.id == "btnStartGame") {
     Dementia.displayGame();
